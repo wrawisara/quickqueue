16d552421fa8d4ba3f38644980b2c5e0d17c0255
wrawisara <rawisara.m@ku.th>
2023-02-16 17:23:31 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/quickqueue/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.quickqueue
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/AllRestaurant.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AllRestaurant{
+  String name;
+  String queueNum;
+  String img;
+
+  AllRestaurant(this.name,this.queueNum,this.img);
+}
+
